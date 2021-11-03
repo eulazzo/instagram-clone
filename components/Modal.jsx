25bea11aed15553b059c30751bf6e01c1890e6a7
Lázaro Vanderson <eulazzo@protonmail.com>
2021-11-03 14:36:06 -0300

    implementing firebase v9

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,23 +1,64 @@
-import { useRecoilState, useSetRecoilState } from "recoil"
+import { Snapshot, useRecoilState, useSetRecoilState } from "recoil"
 import { modalState } from "../atoms/modalAtom"
 import { Dialog, Transition} from '@headlessui/react'
 import { Fragment, useRef, useState } from "react"
 import { CameraIcon } from "@heroicons/react/outline"
+import {db,storage} from '../firebase'
+import { addDoc, collection, doc, serverTimestamp, updateDoc } from "@firebase/firestore"
+import { useSession } from "next-auth/react"
+import { ref,getDownloadURL,uploadString } from "@firebase/storage"
+import { async } from "@firebase/util"
+
 
  
 export default function Modal() {
-
+  const {data:session} = useSession()
   const [ open, setOpen ] = useRecoilState(modalState)
   const filePickerRef = useRef(null)
   const captionRef = useRef(null)
   const [selectedFile,setSelectedFile] = useState(null)
+  const [loading,setLoading] = useState(false)
 
-  const addImageToPost = (event)=>{
+
+  const uploadPost = async ()=>{
+    if(loading) return 
+
+    setLoading(true)
+
+    //1 - Create a post and add to firestore post collection
+    //2 - Get the post id for the newly created post
+    //3 - Upload the image to firebase storage with the post id
+    //4 - Get a download URL from storage and update the original post with image
+
+    const docRef = await addDoc(collection(db,'posts'),{
+      username:session.user.username,
+      caption: captionRef.current.value,
+      profileImg:session.user.image,
+      timestamp:serverTimestamp()
+    })
+
+    console.log('doc added with id',docRef.id);
+
+    const imageRef = ref(storage, `posts/${docRef.id}/image`)
+    await uploadString(imageRef,selectedFile,'data_url')
+      .then(async Snapshot =>{
+        const downloadUrl = await getDownloadURL(imageRef)
+        await updateDoc(doc(db,'posts',docRef.id),{
+          image:downloadUrl
+        })
+      })
+    setOpen(false)
+    setLoading(false)
+    setSelectedFile(null)
+  }
+
+
+  const addImageToPost = event=>{
     const reader = new FileReader()
     if(event.target.files[0]){
       reader.readAsDataURL(event.target.files[0])
     }
-    reader.onload = (readerEvent) =>{
+    reader.onload = readerEvent=>{
       setSelectedFile(readerEvent.target.result)
     }
   }
@@ -102,9 +143,10 @@ export default function Modal() {
               <div className='mt-5 sm:mt-6'>
                 <button type='button' 
                 // disabled={!selectedFile}
+                onClick={uploadPost}
                   className='inline-flex justify-center w-full rounded-md border border-transparent  shadow-sm px-4 py-2 bg-red-600 text-base  font-medium text-white hover:bg-red-700 focus:outline-none  focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hover:disabled:bg-gray-300'
                 >
-                  Upload Image
+                  {loading ? 'uploading...' : 'Upload Image'}
                 </button>
               </div>
             </div>
