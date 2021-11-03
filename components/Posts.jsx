45bf65b7a667680dc7655d3214beb418f148715d
Lázaro Vanderson <eulazzo@protonmail.com>
2021-11-03 15:08:28 -0300

    bulding the post component part II

--- components/Posts.jsx
+++ components/Posts.jsx
@@ -1,37 +1,59 @@
+import { useEffect, useState } from "react";
 import { Post } from "./Post";
-
-const posts =  [
+import {
+  onSnapshot,
+  collection,
+  query,
+  orderBy,
+  serverTimestamp,
+} from "@firebase/firestore";
+import { db } from "../firebase";
+const posts = [
   {
-    id:'1',
-    username:'eulazzo',
-    user_img:'https://thispersondoesnotexist.com/image',
-    img:'https://thispersondoesnotexist.com/image',
-    caption:'This is D.O.P.E!'
+    id: "1",
+    username: "eulazzo",
+    user_img: "https://thispersondoesnotexist.com/image",
+    img: "https://thispersondoesnotexist.com/image",
+    caption: "This is D.O.P.E!",
   },
   {
-    id:'2',
-    username:'eulazzo',
-    user_img:'https://thispersondoesnotexist.com/image',
-    img:'https://thispersondoesnotexist.com/image',
-    caption:'This is D.O.P.E!'
+    id: "2",
+    username: "eulazzo",
+    user_img: "https://thispersondoesnotexist.com/image",
+    img: "https://thispersondoesnotexist.com/image",
+    caption: "This is D.O.P.E!",
   },
-]
-   
+];
 
 const Posts = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+
+    [db]
+  );
+ console.log(posts);
   return (
     <div>
-      {posts.map(({id,username,user_img,caption, img}) => (
-        <Post key={id}
-          id={id}
-          username={username}
-          user_img={user_img}
-          caption={caption}
-          img={img}
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          username={post.data().username}
+          user_img={post.data().profileImg}
+          img={post.data().image }
+          caption={post.data().caption}
         />
       ))}
     </div>
   );
 };
 
-export default Posts  
+export default Posts;
