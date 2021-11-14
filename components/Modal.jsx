211a38f694a055d052d6afec865ce4ec441b628e
Lázaro Vanderson <eulazzo@protonmail.com>
2021-11-14 11:13:47 -0300

    instagram logo assets

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -74,6 +74,7 @@ export default function Modal() {
         className="fixed z-10 inset-0 overflow-y-auto"
         onClose={setOpen}
       >
+         
         <div
           className="flex items-end justify-center min-h-[800px]
         sm:min-h-screen pt-4  px-4 pb-20 text-center sm:block  sm:p-0"
